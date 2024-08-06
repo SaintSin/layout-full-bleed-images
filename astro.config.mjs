cf60ce96880262ab825793e9b7e342da49a41a8c
StJohn <stjohn@me.com>
2024-08-06 10:35:52 +0100

    Add Compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from "astro/config";
 
+import playformCompress from "@playform/compress";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    (await import("@playform/compress")).default({
+      CSS: true,
+      HTML: true,
+      Image: false,
+      JavaScript: true,
+      SVG: true,
+    }),
+  ],
+});
