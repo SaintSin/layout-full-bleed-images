4a878a7c7d6bd6a032140deeac0160f0773f6ebb
StJohn <stjohn@me.com>
2024-09-08 17:52:36 +0100

    Updates, add prettier

--- src/scripts/image.ts
+++ src/scripts/image.ts
@@ -1,42 +1,42 @@
 const min_screen_width = 600;
 
 function handleImages() {
-	// console.log("Function Called");
+  // console.log("Function Called");
 
-	if (window.innerWidth > min_screen_width) {
-		const fullWidthSplitScreens = document.querySelectorAll(
-			".full-width-split-screen",
-		);
+  if (window.innerWidth > min_screen_width) {
+    const fullWidthSplitScreens = document.querySelectorAll(
+      ".full-width-split-screen",
+    );
 
-		for (const element of fullWidthSplitScreens) {
-			const imgElements = element.querySelectorAll("img");
+    for (const element of fullWidthSplitScreens) {
+      const imgElements = element.querySelectorAll("img");
 
-			for (const imgElement of imgElements) {
-				imgElement.style.height = `${element.clientHeight}px`;
+      for (const imgElement of imgElements) {
+        imgElement.style.height = `${element.clientHeight}px`;
 
-				imgElement.addEventListener("load", () => {
-					imgElement.style.height = `${element.clientHeight}px`;
-					// console.log("Image loaded and resized!");
-				});
-			}
-		}
-	}
+        imgElement.addEventListener("load", () => {
+          imgElement.style.height = `${element.clientHeight}px`;
+          // console.log("Image loaded and resized!");
+        });
+      }
+    }
+  }
 
-	// Remove height styling if window goes below minimum width
-	if (window.innerWidth <= min_screen_width) {
-		const fullWidthSplitScreens = document.querySelectorAll(
-			".full-width-split-screen",
-		);
+  // Remove height styling if window goes below minimum width
+  if (window.innerWidth <= min_screen_width) {
+    const fullWidthSplitScreens = document.querySelectorAll(
+      ".full-width-split-screen",
+    );
 
-		for (const element of fullWidthSplitScreens) {
-			const imgElement = element.querySelector(
-				"img",
-			) as HTMLImageElement | null;
-			if (imgElement) {
-				imgElement.style.height = "";
-			}
-		}
-	}
+    for (const element of fullWidthSplitScreens) {
+      const imgElement = element.querySelector(
+        "img",
+      ) as HTMLImageElement | null;
+      if (imgElement) {
+        imgElement.style.height = "";
+      }
+    }
+  }
 }
 
 document.addEventListener("astro:page-load", handleImages);
