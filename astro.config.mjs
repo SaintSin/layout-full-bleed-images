42327490b1ed84a3d6f941ce1f470007667bf9e4
StJohn <stjohn@me.com>
2025-06-03 09:15:51 +0100

    Prefetch and compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
 import { defineConfig } from "astro/config";
 
+import playformCompress from "@playform/compress";
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     responsiveImages: true,
   },
-  integrations: [],
+  prefetch: true,
+  integrations: [
+    (await import("@playform/compress")).default({
+      CSS: true,
+      HTML: true,
+      Image: false,
+      JavaScript: true,
+      JSON: true,
+      SVG: false,
+    }),
+  ],
 });
