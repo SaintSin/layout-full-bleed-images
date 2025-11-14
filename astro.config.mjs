cde4b2d22f468b0fd1f29c490177b3a21c949670
StJohn <stjohn@me.com>
2025-11-14 14:30:11 +0000

    Formatting, CSS layers, better OG meta, AstroCapo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,19 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'http://localhost:4321',
-	experimental: {},
-	prefetch: true,
-	integrations: [
-		sitemap(),
-		icon(),
-		(await import('@playform/compress')).default({
-			CSS: true,
-			HTML: true,
-			Image: false,
-			JavaScript: true,
-			JSON: true,
-			SVG: false,
-		}),
-	],
+  site: 'http://localhost:4321',
+  experimental: {},
+  prefetch: true,
+  integrations: [
+    sitemap(),
+    icon(),
+    (await import('@playform/compress')).default({
+      CSS: true,
+      HTML: true,
+      Image: false,
+      JavaScript: true,
+      JSON: true,
+      SVG: false,
+    }),
+  ],
 });
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,12 @@
 export interface MetaData {
-	title: string;
-	description: string;
-	image?: string;
-	canonicalURL?: URL | string;
+  title: string;
+  description: string;
+  image?: string;
+  imageOG?: string;
+  altOG?: string;
+  canonicalURL?: URL | string;
 }
 
 export interface PageProps {
-	metaData: MetaData;
+  metaData: MetaData;
 }
